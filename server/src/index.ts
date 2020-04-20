0fd47857ee99bf7c352a58189dc4671a097f9a18
Brian Nguyen <vanpho01@gmail.com>
2020-04-20 19:09:09 +0700

    Run on heroku

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,11 @@
 /* istanbul ignore file */
-import { Env, EEnvKey } from './global'
+import { Env, EEnvKey, runMigrations } from './global'
 import { app } from './app'
 import { RouteLoader } from './app-loader/route-loader'
 import { scan } from './scanner'
 
 async function start() {
+  await runMigrations()
   await RouteLoader.load(app)
   app.listen(Env.get(EEnvKey.PORT), () => console.log('Server started.'))
   scan()
